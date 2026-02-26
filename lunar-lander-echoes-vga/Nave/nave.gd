f74378bc3e0ffcca57ebbc890e7096cb4e00bf0c
MermeladaMadre <astucia.huitzilopochtli@gmail.com>
2026-02-25 22:02:09 -0600

    Clase 2

--- lunar-lander-echoes-vga/Nave/nave.gd
+++ lunar-lander-echoes-vga/Nave/nave.gd
@@ -1,9 +1,10 @@
 class_name Nave 
 extends RigidBody3D
 
-@export var impulso : float = 2
-@export var inclinacion : float = 1
+@export var impulso : float = 10
+@export var inclinacion : float = 100
 @export var combustible : float = 1000
+@export var consumo : float = 10
 @export var escudo : int = 1
 
 # Called when the node enters the scene tree for the first time.
@@ -12,6 +13,31 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("motor"):
-		apply_central_impulse(Vector3.UP * impulso * delta)
+	if Input.is_action_pressed("motor") and combustible > 0:
+		apply_central_impulse(basis.y * impulso * delta)
+		combustible = clamp(combustible - (consumo * delta),0,combustible)
+		
+		#printt("Combustible: ",combustible)
+	
+	
+	if Input.is_action_pressed("rotacion_left"):
+		apply_torque(Vector3(0,0,inclinacion * delta))
+		
+	if Input.is_action_pressed("rotacion_right"):
+		apply_torque(Vector3(0,0,-inclinacion * delta))
+	
 	pass
+
+
+func _on_body_entered(body: Node) -> void:
+	print(body.get_groups())
+	if body.is_in_group("Victoria"):
+		print("Victoria")
+	elif body.is_in_group("Crash"):
+		print("Game Over")
+		get_tree().reload_current_scene()
+	elif body.is_in_group("Bonus"):
+		print("Bonus")
+	
+	
+	pass # Replace with function body.
